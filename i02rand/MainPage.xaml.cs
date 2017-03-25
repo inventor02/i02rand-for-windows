f2ebc3e7dec3d6e7e7bd17566d39991d3322dbc2
George Peppard <george@inventor02.me>
2017-03-25 15:08:37 +0000

    hamburger menu [RANDW-1]

--- i02rand/MainPage.xaml.cs
+++ i02rand/MainPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using MetroLog;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Services.Store;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +24,56 @@ namespace i02rand
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private ILogger logger = LogManagerFactory.DefaultLogManager.GetLogger<MainPage>();
+
+        StoreContext context = null;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        public async void DisplayGenericDialog(string title, string message)
+        {
+            ContentDialog dialog = new ContentDialog()
+            {
+                Title = title,
+                Content = message,
+                PrimaryButtonText = "Roger"
+            };
+
+            ContentDialogResult result = await dialog.ShowAsync();
+        }
+
+        public async void GetAddOnInfo()
+        {
+            if (context == null)
+            {
+                context = StoreContext.GetDefault();
+            }
+
+            string[] productKinds = {"Durable", "Consumable", "UnmanagedConsumable"};
+            List<String> filterList = new List<string>(productKinds);
+
+            StoreProductQueryResult queryResult = await context.GetAssociatedStoreProductsAsync(filterList);
+
+            if (queryResult.ExtendedError != null)
+            {
+                logger.Error("Could not get add-on info: " + queryResult.ExtendedError.Message);
+                return;
+            }
+
+            foreach (KeyValuePair<string, StoreProduct> item in queryResult.Products)
+            {
+                StoreProduct product = item.Value;
+            }
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            HamburgerSplitView.IsPaneOpen = !HamburgerSplitView.IsPaneOpen;
+
+            DisplayGenericDialog("Hold up", "")
         }
     }
 }
